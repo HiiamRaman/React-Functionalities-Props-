09c4df260905cd511e70f16d4df4d5616c532915
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2025-01-02 22:51:24 +0545

    intial done

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
 import { useState } from 'react'
 
 import './App.css'
-
+import  Devio from './Components/Devui'
 export default  function App() {
+
  
+let Add={
+  sum:"a+b"
+}
   return (
-  <>
-<h1 className='bg-green-300 text-gray-950 p-5 rounded-xl mb-8'> Tailwind is working or not??</h1>
-<div className="flex flex-col rounded-xl  p-4"
-        style={{
-          border: '0.88px solid',
+  <div>
+    <h1 className='bg-green-300 text-gray-950 p-5 rounded-xl mb-8'> Tailwind is working or not?? </h1>
+
+     <Devio name="Raman Singh" age={23}  />
+     <Devio />
+  </div>
+
   
-          backdropFilter: 'saturate(180%) blur(14px)',
-          background: ' #ffffff0d',
-        }}
-      >
-        <div>
-          <img
-            src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
-            alt="nft-gif"
-            width="400"
-            height="400"
-            className="rounded-xl"
-          />
-        </div>
-        <div className="flex flex-col  rounded-b-xl py-4 ">
-          <div className="flex justify-between">
-            
-            <h1 className="font-bold font-RubikBold">Alien</h1>
-          </div>
-          
-        </div>
-      </div>
-  </>
 
   )
 }
